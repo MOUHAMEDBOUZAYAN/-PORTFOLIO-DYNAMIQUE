2efb4379d99f5fecc5031edc7df703256a6cfcd0
mouhamedbouzayan <bouzimohammed177@gmail.com>
2025-01-17 17:47:14 +0100

    final portfolio

--- script.js
+++ script.js
@@ -31,4 +31,45 @@ $(document).ready(function () {
         let $header = $('header');
         $header.toggleClass('sticky', $(this).scrollTop() > 100);
     });
+
+/*======================= remove toggle icon and navebare when click navbar link ( scrol)   ============================*/
+$(this).removeClass('fa-solid fa-x');
+$navbar.removeClass('active');
+
+
+
 });
+
+/*======================= scroll reveal ============================*/
+ScrollReveal({
+    // reset:true,
+    distance :'80px',
+    duration:2000,
+    delay:200
+});
+
+ScrollReveal().reveal('.home-content, .heading',{ origin:'top'});
+ScrollReveal().reveal('.home-img, .services-container, .portfolio-box , .contact form',{ origin:'bottom'});
+ScrollReveal().reveal('.home-content h1, .about-img',{ origin:'left'});
+ScrollReveal().reveal('.home-content p, .about-content',{ origin:'right'});
+
+/*======================= typed js ============================*/
+var typed = new Typed(".text-multiple", {
+    strings:["Devlopeur Full-stack" , "designer" , "trader-analisyc"] ,
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+});
+function getElementByIdAndCheck(name) {
+    if (!name) {
+        alert("leoo");
+    } else {
+        return document.getElementById(name);
+    }
+}
+
+
+
+
+
